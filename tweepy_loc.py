5c7cd344c1ff679235a0d93d4380516bb396d46e
JiayunDeng <55669518+JiayunDeng@users.noreply.github.com>
2020-02-24 11:13:52 +0000

    updated tweeps_loc.py

--- tweepy_loc.py
+++ tweepy_loc.py
@@ -1,11 +1,17 @@
-from tweepy.streaming import StreamListener
+from tweepy import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from geopy.geocoders import Nominatim
 
+import json
 import gmplot
 import twitter_credentials
 
+# Authenticate using config.py and connect to Twitter Streaming API.
+location = [-74.1687,40.5722,-73.8062,40.9467]
+track = ['Trump', 'ocean', 'Democrat']
+languages = ['en']
+fetched_tweets_filename = "tweets_location_r.txt"
 
 # # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
@@ -17,18 +23,17 @@ class TwitterStreamer():
 
     def stream_tweets(self, fetched_tweets_filename, location):
         # This handles Twitter authetification and the connection to Twitter Streaming API
-        listener = StdOutListener(fetched_tweets_filename)
+        listener = Listener(fetched_tweets_filename)
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
         stream = Stream(auth=auth, listener=ListenerChild(api=None,fetched_tweets_filename=fetched_tweets_filename),wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
 
         # This line filter Twitter Streams to capture data by the keywords: 
-        stream.filter(locations=region,languages=languages,track=track)
+        stream.filter(locations=location)
 
  # # # # TWITTER STREAM LISTENER # # # #       
 class Listener(StreamListener):
-
       1 == 1;
 
 class ListenerChild(Listener):
@@ -41,7 +46,7 @@ class ListenerChild(Listener):
         j = json.loads(data)
         try:
             if j['geo'] is not None:
-                tt = parseTweet(j)
+                tt = parseTweets(j, simplify = FALSE, verbose = TRUE, legacy = FALSE)
                 logging.info(tt)
                 logging.info (tt)
                 self.producer.send('tweets', bytearray(tt,'utf-8'))
@@ -52,12 +57,7 @@ class ListenerChild(Listener):
         print(status)
 
 if __name__ == '__main__':
- 
-    # Authenticate using config.py and connect to Twitter Streaming API.
-    location = [51.5074, 0.1278]
-    track = ['Trump', 'ocean', 'Democrat']
-    languages = ['en']
-    fetched_tweets_filename = "tweets_location_r.txt"
+    
 
     twitter_streamer = TwitterStreamer()
     twitter_streamer.stream_tweets(fetched_tweets_filename, location)
